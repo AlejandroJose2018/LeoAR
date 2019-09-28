3a334a812ea6517462e6e0b03a9bf5df4b8613af
Ali Tasci <ali-tasci@hotmail.com>
2019-09-28 19:35:02 +0300

    download anims from server

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -10,8 +10,9 @@ using UnityEngine.UI;
 public class CharacterManager : MonoBehaviour
 {
     public RuntimeAnimatorController Ani;
-    public string mainJson,url, texture;
-    public GameObject characterContent,characterPrefab;
+    public string mainJson;
+    public GameObject characterContent,characterPrefab,danceContent,dancePrefab;
+    
 
     IEnumerator Start()
     {
@@ -24,10 +25,7 @@ public class CharacterManager : MonoBehaviour
         }
         else
         {
-            Debug.Log(www.downloadHandler.text);
             var test = JSON.Parse(www.downloadHandler.text);
-            print(test.Count);
-            print(test["characters"].Count);
             int indexer = 0;
             foreach (var to in test["characters"])
             {
@@ -48,12 +46,10 @@ public class CharacterManager : MonoBehaviour
 
        
     }
-    public void DownloadCharacter()
-    {
-        StartCoroutine(DownCor(url, texture));
-    }
+    
     AssetBundle bundle;
     AssetBundle textureBundle;
+    AssetBundle danceBundle;
     GameObject selectedObj;
     IEnumerator DownloadCor(JSONNode jSON)
     {
@@ -61,36 +57,37 @@ public class CharacterManager : MonoBehaviour
         {
             bundle.Unload(false);
         }
+
         if (textureBundle != null)
         {
             textureBundle.Unload(false);
         }
 
+        if (danceBundle != null)
+        {
+            danceBundle.Unload(false);
+        }
+
         var uwr = UnityWebRequestAssetBundle.GetAssetBundle(jSON["prefaburl"]);
         yield return uwr.SendWebRequest();
-
-        // Get an asset from the bundle and instantiate it.
         bundle = DownloadHandlerAssetBundle.GetContent(uwr);
-        print(bundle.name);
+
         var ast = bundle.LoadAsset<GameObject>(bundle.name);
+
         if (selectedObj != null)
             Destroy(selectedObj);
 
         selectedObj = Instantiate(ast, new Vector3(0, 0, 0), Quaternion.identity);
-        selectedObj.GetComponent<Animator>().runtimeAnimatorController = Ani;
         print(selectedObj.name);
 
         var uwrTexture = UnityWebRequestAssetBundle.GetAssetBundle(jSON["textureurl"]);
         yield return uwrTexture.SendWebRequest();
-
         textureBundle = DownloadHandlerAssetBundle.GetContent(uwrTexture);
 
-
-        //var modelRenderer = gameobj.transform.Find(jSON["prefabobj"] + " (Instance)").gameObject.GetComponent<SkinnedMeshRenderer>();
+        
         var modelRenderer = selectedObj.transform.Find(jSON["prefabobj"]).gameObject.GetComponent<SkinnedMeshRenderer>();
         var materialList = modelRenderer.materials.ToList();
-
-       // var testmat = materialList.Find(item => item.name == "asd");
+        
         foreach (var matt in modelRenderer.materials)
         {
             matt.EnableKeyword("_MainTex");
@@ -106,10 +103,8 @@ public class CharacterManager : MonoBehaviour
                 {
                     print(texture.Value["texturename"] + "  ---  " + mm);
                 }
-                print(tempmat);
                 var tempText = textureBundle.LoadAssetAsync<Texture>(texture.Value["textureaddress"]);
                 yield return tempText;
-                print(tempText);
                 tempmat.SetTexture("_MainTex", tempText.asset as Texture);
             }
             else if (texture.Value["texturetype"] == "normal")
@@ -119,58 +114,38 @@ public class CharacterManager : MonoBehaviour
                 {
                     print(texture.Value["texturename"] + " (Instance)" + "  ---  " + mm + (texture.Value["texturename"] + " (Instance)").Equals(mm));
                 }
-                print(tempmat);
-                //var tempmat = materialList.Find(item => item.name == texture.Value["texturename"]);
                 var tempText = textureBundle.LoadAssetAsync<Texture>(texture.Value["textureaddress"]);
                 yield return tempText;
                 tempmat.SetTexture("_NORMALMAP", tempText.asset as Texture);
             }
         }
-       /* var mainTex = textureBundle.LoadAssetAsync<Texture>("Cooper_Body_Diff");
-        yield return mainTex;
-        modelRenderer.materials[1].SetTexture("_MainTex", mainTex.asset as Texture);
 
-        var normTex = textureBundle.LoadAssetAsync<Texture>("Cooper_Body_Normal");
-        yield return normTex;
-        modelRenderer.materials[2].SetTexture("_NORMALMAP", normTex.asset as Texture);*/
-    }
+        var uwrDance = UnityWebRequestAssetBundle.GetAssetBundle(jSON["animationurl"]);
+        yield return uwrDance.SendWebRequest();
+        danceBundle = DownloadHandlerAssetBundle.GetContent(uwrDance);
+        int indexer = 0;
 
-    IEnumerator DownCor(string url, string urlTexture)
-    {
-        var uwr = UnityWebRequestAssetBundle.GetAssetBundle(url);
-        yield return uwr.SendWebRequest();
-
-        // Get an asset from the bundle and instantiate it.
-        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
-        print(bundle.name);
-        var ast = bundle.LoadAsset<GameObject>(bundle.name);
-
-        var gameobj = Instantiate(ast, new Vector3(0, 0, 0), Quaternion.identity);
-        gameobj.GetComponent<Animator>().runtimeAnimatorController = Ani;
-        print(gameobj.name);
-        
-        var uwrTexture = UnityWebRequestAssetBundle.GetAssetBundle(urlTexture);
-        yield return uwrTexture.SendWebRequest();
-
-        AssetBundle textureBundle = DownloadHandlerAssetBundle.GetContent(uwrTexture);
-
-        
-        
-        var modelRenderer = gameobj.transform.Find("CHAR_Cooper").gameObject.GetComponent<SkinnedMeshRenderer>();
-        var materialList = modelRenderer.materials.ToList();
-
-        var testmat = materialList.Find(item => item.name == "asd");
-        foreach (var matt in modelRenderer.materials)
+        foreach(var dance in jSON["animatons"])
         {
-            matt.EnableKeyword("_MainTex");
-            matt.EnableKeyword("_NORMALMAP");
+            var danceAnim = danceBundle.LoadAssetAsync<RuntimeAnimatorController>(dance.Value["address"]);
+            yield return danceAnim;
+            
+            var danceNew = Instantiate(dancePrefab);
+            danceNew.transform.SetParent(danceContent.transform);
+            danceNew.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 160);
+            danceNew.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -(indexer * 220 + 180));
+            danceNew.transform.Find("text").GetComponent<TextMeshProUGUI>().text = dance.Value["name"];
+            danceNew.name = dance.Value["name"];
+            danceNew.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                selectedObj.GetComponent<Animator>().runtimeAnimatorController = danceAnim.asset as RuntimeAnimatorController;
+            });
+            if (indexer == 0)
+            {
+                selectedObj.GetComponent<Animator>().runtimeAnimatorController = danceAnim.asset as RuntimeAnimatorController;
+            }
+            indexer++;
         }
-        var mainTex = textureBundle.LoadAssetAsync<Texture>("Cooper_Body_Diff");
-        yield return mainTex;
-        modelRenderer.materials[1].SetTexture("_MainTex", mainTex.asset as Texture);
-
-        var normTex = textureBundle.LoadAssetAsync<Texture>("Cooper_Body_Normal");
-        yield return normTex;
-        modelRenderer.materials[2].SetTexture("_NORMALMAP", normTex.asset as Texture);
+        
     }
 }
