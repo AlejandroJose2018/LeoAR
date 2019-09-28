cbe00dfa4c325ef16bf7fa84b12d3a465fae9575
Ali Tasci <ali-tasci@hotmail.com>
2019-09-28 13:48:12 +0300

    prepare assetbundle

---
+++ Assets/Editor/CreateAssets.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssets : MonoBehaviour
+{
+    [MenuItem("Assets/Build Asset Bundles")]
+    static void BuildABs()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/ABs/Android", BuildAssetBundleOptions.None, BuildTarget.Android);
+        //BuildPipeline.BuildAssetBundles("Assets/ABs/iOS", BuildAssetBundleOptions.None, BuildTarget.iOS);
+    }
+}
